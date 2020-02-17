7b9ed6dadf102edab23b21a43fe624bb4e192457
Oliver Baker <oliver.baker@ocsbstudent.ca>
2020-02-17 10:16:19 -0500

    Revert "Merge branch 'Oliver's-Branch' of https://github.com/OllyBaker/2994-Scouting-App into Oliver's-Branch"
    This reverts commit df29f2f841dc57e61d2ad41df35be5941aa3b176, reversing
    changes made to 7f3ee8f1de73fa0fcf16acebb1c783b7c285ebcf.

--- desktopSide/public/datamap.js
+++ desktopSide/public/datamap.js
@@ -1,61 +1,80 @@
-const dataNames = {
-	startLevel: ["startlvl"],
-	cargo: ["cargo1", "cargo2", "cargo3", "cargo4"],
-	hatch: ["hatch1", "hatch2", "hatch3", "hatch4"],
-	rocketCargo: ["rock1c", "rock2c", "rock3c"],
-	rocketHatch: ["rock1h", "rock2h", "rock3h"],
-	shipCargo: ['shipc'],
-	shipHatch: ['shiph'],
+export const dataNames = {
+	crossedInitiation: ["crossedinitiation"],
+	teamColour: ['Blue', 'Red'],
+	colourChoice: ['colChoice'],
 	climbing: {
-		levelReached: "climblvlReached",
-		assist: "assist"
+		ableToClimb: "abletoclibm",
+		hangingMobility: "hangingMobility",
+		assist: "assist",
+		balanced: "balance"
 	},
+	shooting: {
+		autoLow: ['lowA'],
+		autoHigh: ['highA'],
+		autoMissed: ['missA'],
+		autoBlocked: ["blockedA"],
+		teleLow: ['lowT'],
+		teleHigh: ['highT'],
+		teleMissed: ['missT'],
+		teleBlocked: ["blockedT"]
+	},
+	controlPanel: {
+		rotationControl: ["rotationCP"],
+		positionControl: ["positionCP"]
+	},
+
+	powerCellPickup: {
+		fromGround: ["groundPCP"],
+		fromLoading: ["loadingPCP"]
+	},
+
 	attributes: {
 		broken: "broken",
 		tip: "tip",
 		cargoFromDepot: "depot",
-		hatchesFromFloor: "floor"
+		hatchesFromFloor: "floor",
+		moveTrench: "trench",
+		powerCellCapacity: "pc-capacity"
 	},
 	gameInfo: {
 		opposingSideTime: "opposide",
 		penaltyPoints: "ppoints",
-		hatchesDropped: "hdropped"
+		fuelCellsDropped: "hdropped"
+	},
+	timingInfo: {
+		parkingTime: "parktimeGI",
+		climbingTime: "climbtimeGI",
+		rotationTime: "rotationGI",
+		positionTime: "positionGI"
 	},
 	matchInfo: {
 		matchNumber: "matchNumber",
 		teamNumber: "teamNumber"
+	},
+	gameNotes: {
+		autoNotes: "notes-autonomous",
+		teleNotes: "notes-teleop",
+		climbNotes: "notes-endgame",
+		overallNotes: "noteO",
+		timeRemainingHung: "note-time-remaining"
 	}
 }
 
-const climbOptions = [
+export const threeOptions = [
+	"Not Observed",
+	"Yes",
+	"No"
+]
+
+export const defaultThreeOptions = 0;
+
+export const climbOptions = [
 	"No climb",
 	"Level 1",
 	"Level 2",
 	"Level 3"
 ]
-const defaultClimbOption = 0;
-
-const assistOptions = [
-	"No assist",
-	"Level 2",
-	"Level 3"
-]
-
-const startLevelOptions = [
-	"Didn't cross baseline",
-	"Level 1",
-	"Level 2"
-]
-const defaultAssistOption = 0;
-
-const gamePieceOptions = [
-	"None",
-	"Rocket level 1",
-	"Rocket level 2",
-	"Rocket level 3",
-	"Cargo Ship"
-]
-const defaultGamePieceOption = 0;
+export const defaultClimbOption = 0;
 
 function swap(json) {
 	let ret = {};
@@ -64,56 +83,25 @@ function swap(json) {
 	}
 	return ret;
 }
-const dataTypes = {
-	"startLevel": swap(startLevelOptions),
-	"cargo": swap(gamePieceOptions),
-	"hatch": swap(gamePieceOptions),
-	"rocketCargo": [0, 1, 2, 3, 4],
-	"rocketHatch": [0, 1, 2, 3, 4],
-	"shipCargo": [0, 1, 2, 3, 4, 5, 6, 7, 8],
-	"shipHatch": [0, 1, 2, 3, 4, 5, 6, 7, 8],
+export const dataTypes = {
+	"crossedInitiation": [0, 1],
 	"climbing": { ...swap(climbOptions), "No assist": 0 },
 	"attributes": [0, 1],
 	"gameInfo": "number",
 	"matchInfo": "number"
 }
-
-const bitmap = {
-	"startLevel": {
-		bits: 2,
-		amount: 1
-	},
-	"cargo": {
-		bits: 3,
-		amount: 4
-	},
-	"hatch": {
-		bits: 3,
-		amount: 4
-	},
-	"rocketCargo": {
-		bits: 3,
-		amount: 3
-	},
-	"rocketHatch": {
-		bits: 3,
-		amount: 3
-	},
-	"shipCargo": {
-		bits: 4,
-		amount: 1
-	},
-	"shipHatch": {
-		bits: 4,
-		amount: 1
+export const bitmap = {
+	"crossedInitiation": {
+		bits: 1,
+		amounts: 1,
 	},
 	"climbing": {
-		bits: 2,
-		amount: 2
+		bits: 8,
+		amount: 4
 	},
 	"attributes": {
-		bits: 1,
-		amount: 4
+		bits: 4,
+		amount: 6
 	},
 	"gameInfo": {
 		bits: 8,
@@ -124,10 +112,3 @@ const bitmap = {
 		amount: 2
 	}
 }
-
-
-const dataMap = {
-	bitmap, dataNames, dataTypes, defaultAssistOption, defaultGamePieceOption, defaultClimbOption
-}
-
-module.exports = dataMap;
